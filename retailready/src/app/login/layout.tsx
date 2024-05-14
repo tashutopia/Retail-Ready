5a1d7a8daee832b65bc097cf0ee0e48ec20bdb95
tashutopia <tashutopia@gmail.com>
2024-05-14 13:34:51 -0400

    pls work pt2

--- retailready/src/app/login/layout.tsx
+++ retailready/src/app/login/layout.tsx
@@ -1,13 +1,7 @@
-import { BrowserRouter as Router } from "react-router-dom";
-
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <Router>
-      <div>{children}</div>
-    </Router>
-  );
+  return <div>{children}</div>;
 }
