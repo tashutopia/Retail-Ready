112e934cfafbc693e6491ef5628c63d14e65f738
tashutopia <tashutopia@gmail.com>
2024-05-15 18:05:50 -0400

    just cleaned it up a bit

--- retailready/src/app/login/authenticate.ts
+++ retailready/src/app/login/authenticate.ts
@@ -1,20 +1,14 @@
 import { signIn } from "./auth";
 import { AuthError } from 'next-auth';
 
-
 export async function authenticate(
     prevState: string | undefined,
     formData: FormData,
 ) {
+
     try {
         const response = await signIn('credentials', formData);
-        if (response) {
-            console.log("RESPONSE AHDHASDIOFHASJKDF")
-            window.location.href = '/home';
-        } else {
-            console.log(response)
-            window.location.href = '/home';
-        }
+        window.location.href = '/home';
     } catch (error) {
         if (error instanceof AuthError) {
             switch (error.type) {
